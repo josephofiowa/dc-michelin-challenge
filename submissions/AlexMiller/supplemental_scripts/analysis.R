cc76364922271da96a885cf63cd226cd82fd6308
Alex Miller <alex.k.miller@gmail.com>
2016-10-06 22:33:54 -0400

    population analysis

--- submissions/AlexMiller/supplemental_scripts/analysis.R
+++ submissions/AlexMiller/supplemental_scripts/analysis.R
@@ -6,13 +6,21 @@ setwd(wd)
 dat <- read.csv("ny_stars.csv")
 latest <- subset(dat,year==2016 & stars>0)
 ny.ratio <- nrow(latest)/190
+ny.pop <- 8.406
+ny.starred.percap <- nrow(latest)/ny.pop
 
 dat <- read.csv("chi_stars.csv",na.strings="Closed")
 latest <- subset(dat,year==2016 & stars>0)
 chi.ratio <- nrow(latest)/58
+chi.pop <- 2.719
+chi.starred.percap <- nrow(latest)/ny.pop
 
-average <- mean(c(chi.ratio,ny.ratio))
+average.bib <- mean(c(chi.ratio,ny.ratio))
+average.pop <- mean(c(chi.starred.percap,ny.starred.percap))
 
 dc.bib <- 19
-dc.stars <- dc.bib*average
+dc.pop <- 0.658893
+dc.stars.bib <- dc.bib*average.bib
+dc.stars.pop <- dc.pop*average.pop
+dc.stars.pop.like.ny <- dc.pop*ny.starred.percap
 #Perhaps 7-8 starred restaurants?
